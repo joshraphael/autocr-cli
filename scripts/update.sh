a366de2476c73d5f7e46cfe98dcfce6c9af86af3
Joshua Raphael <josh.n.raphael@gmail.com>
2025-01-21 18:00:25 -0700

    update script

--- scripts/update.sh
+++ scripts/update.sh
@@ -5,5 +5,5 @@ export SHA="0a1dd5dfb87e8293c575641b321dcfab286c0cd3"
 
 for file in "achievements.js" "logic.js" "feedback.js"; do
     rm -f ${file}
-	wget -O "${file}" "${URL}/${SHA}/AutoCR/js/${file}"
+    wget -O "${file}" "${URL}/${SHA}/AutoCR/js/${file}"
 done
