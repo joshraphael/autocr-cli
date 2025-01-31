c714deff234f6f4a0697ff9d4f10c661e83aa2d3
Joshua Raphael <josh.n.raphael@gmail.com>
2025-01-31 00:01:57 -0700

    update to commit 26a88d95a5181ed0db0b213abfc5a488a14fcb3e files

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export URL="https://raw.githubusercontent.com/authorblues/retroachievements"
-export SHA="275fd6713e3dcad6338acd4fe0d519d9ff726bec"
+export SHA="26a88d95a5181ed0db0b213abfc5a488a14fcb3e"
 
 for file in "achievements.js" "logic.js" "feedback.js"; do
     rm -f ${file}
