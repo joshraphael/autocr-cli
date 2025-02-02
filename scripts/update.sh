d01ec0bf24b9f1370020dc708437b0dd3c309072
Joshua Raphael <josh.n.raphael@gmail.com>
2025-02-02 10:55:09 -0700

    update to commit a17f7914562d966c10796da9a8632af77b02c427 files

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 export URL="https://raw.githubusercontent.com/authorblues/retroachievements"
-export OLD_SHA="26a88d95a5181ed0db0b213abfc5a488a14fcb3e"
-export NEW_SHA="124c3d5006f19248747cde3fbdb636cf40decf1c"
+export OLD_SHA="124c3d5006f19248747cde3fbdb636cf40decf1c"
+export NEW_SHA="a17f7914562d966c10796da9a8632af77b02c427"
 
 export MSG=$(git log -1 --pretty=%B)
 if [[ $MSG != "update to commit $OLD_SHA files" ]]; then
