86a4c5df1d50b49d42e953aba40d08bcdbaec966
Joshua Raphael <josh.n.raphael@gmail.com>
2025-02-17 18:41:28 -0700

    update to commit 1520297e9dec7644272a426ec8a2f6723b4883ef files

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 export URL="https://raw.githubusercontent.com/authorblues/retroachievements"
-export OLD_SHA="124c3d5006f19248747cde3fbdb636cf40decf1c"
-export NEW_SHA="a17f7914562d966c10796da9a8632af77b02c427"
+export OLD_SHA="a17f7914562d966c10796da9a8632af77b02c427"
+export NEW_SHA="1520297e9dec7644272a426ec8a2f6723b4883ef"
 
 export MSG=$(git log -1 --pretty=%B)
 if [[ $MSG != "update to commit $OLD_SHA files" ]]; then
@@ -17,7 +17,9 @@ done
 
 if git status --porcelain | grep -q .; then
     echo "Committing changes"
+    git status
     git add -A
+    git status
     git commit -m "update to commit $NEW_SHA files"
 else
     echo "No changes found"
